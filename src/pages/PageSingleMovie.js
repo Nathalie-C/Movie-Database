53046a3327cda24591f7ca4a05c7d1aacdd36ad9
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2024-01-19 15:01:10 -0800

    ok

--- src/pages/PageSingleMovie.js
+++ src/pages/PageSingleMovie.js
@@ -17,7 +17,7 @@ function PageSingleMovie() {
         setMovieData(data);
         const youtubeTrailerVideos = filterVideos(data.videos.results);
         setMovieData(data);
-        setMovieVideos(youtubeTrailerVideos);
+        setMovieVideos(youtubeTrailerVideos); // Fix this line
       })
       .catch((error) => {
         alert(error);
