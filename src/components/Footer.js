fca97cd35f8284e0695013628ea638ff4c06605f
arielle-m <ariem.marii@gmail.com>
2024-01-22 16:40:25 -0800

    added logo to footer, made footer semantic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,31 @@
 export default function Footer() {
   return (
-    <div className="footer">
+    <footer>
       <div className="footer-1st-part">
-        <div className="footer-logo-group">
+        <a className="footer-logo-group site-logo">
           {/* logo */}
-          <img src="" alt="Website logo"></img>
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512">
+            <path d="M256.742 37C407.512 37 501.293 355.711 501.293 355.711C531.717 408.289 493.682 474 432.826 474H79.1676C18.3185 474 -19.717 408.289 10.7069 355.711C10.7069 355.711 91.2167 37 256.742 37Z"></path>
+          </svg>
           <h3>guMDrop</h3>
-        </div>
+        </a>
 
         <ul className="footer-nav">
           <li>
-            <a href="../pages/PageHome.js">HOME</a>
+            <a href="../pages/PageHome.js">Home</a>
           </li>
           <li>
-            <a href="../pages/PageAbout.js">ABOUT</a>
+            <a href="../pages/PageAbout.js">About</a>
           </li>
           <li>
-            <a href="#">FAVOURITES</a>
+            <a href="#">Favourites</a>
           </li>
         </ul>
       </div>
 
       <div className="copyright">
-        <p>&#169; 2024 Arielle M., John S., Nathalie C.</p>
+        <p>&copy; 2024 Arielle M., John S., Nathalie C.</p>
       </div>
-    </div>
+    </footer>
   );
 }
