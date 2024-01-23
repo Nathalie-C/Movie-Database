7a33955836c058ac32bc3ed4b1ea494f73c85005
Nathalie Chang <nathalie.ch.chang@gmail.com>
2024-01-22 16:29:41 -0800

    create banner componenet and update about page

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,20 +1,28 @@
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+
 export default function PageAbout() {
   return (
     <div>
-      <img src="" alt=""></img>
-      <h3>guMDrop</h3>
-      <div>
-        <a href="">
-          <p>HOME</p>
-        </a>
-        <a href="">
-          <p>ABOUT</p>
-        </a>
-        <a href="">
-          <p>FAVOURITES</p>
-        </a>
-      </div>
-      <p>&#169; 2023 Arielle M., John S., Nathalie C.</p>
+      <Banner
+        src="../images/header-slider-img-01.jpg"
+        alt="about banner image"
+      />
+      <h1>guMDrop</h1>
+      <h2>The sweetest way to find movies!</h2>
+      <p>
+        guMDrop is a movie database that helps you find the perfect movie for
+        any occasion. Whether you are looking for a comedy, a thriller, a
+        romance, or anything in between, guMDrop has it all! You can browse
+        through thousands of movies, read reviews, watch trailers, and get
+        recommendations based on your preferences. Let’s satisfy that sweet
+        tooth with a great movie!
+      </p>
+
+      <p>
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+      </p>
+      <Footer />
     </div>
   );
 }
