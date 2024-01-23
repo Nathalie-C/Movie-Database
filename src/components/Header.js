b569392362f667d006ce60a34a635e995cf60fa8
arielle-m <ariem.marii@gmail.com>
2024-01-22 16:39:36 -0800

    created header and nav

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+// import Nav from "./Nav";
+
+export default function Header() {
+    return (
+      <header>
+          <a className="site-logo">
+            {/* logo */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512">
+              <path d="M256.742 37C407.512 37 501.293 355.711 501.293 355.711C531.717 408.289 493.682 474 432.826 474H79.1676C18.3185 474 -19.717 408.289 10.7069 355.711C10.7069 355.711 91.2167 37 256.742 37Z"></path>
+            </svg>
+            <h3>guMDrop</h3>
+          </a>
+  
+          {/* <Nav /> */}
+      </header>
+    );
+}
