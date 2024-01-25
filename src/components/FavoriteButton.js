d081662d93214ae3353aaac6a28212bd2e8265e8
arielle-m <ariem.marii@gmail.com>
2024-01-25 09:11:55 -0800

    added favourite icon; moved .menu-toggle to _icons.scss

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -23,7 +23,10 @@ function FavouriteButton({ movieData }) {
       onClick={handleFavorite}
       className={`favorite ${isFavorited ? "favorited" : ""}`}
     >
-      placeholder
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+        <title>Favourite icon</title>
+        <path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z"></path>
+      </svg>
     </button>
   );
 }
