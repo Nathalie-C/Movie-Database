61640d72dd11e8b5be7a2680d29f64026d4ff62d
arielle-m <ariem.marii@gmail.com>
2024-01-26 11:59:16 -0800

    fixed nav link menu onlick

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,16 +5,15 @@ import { Link } from "react-router-dom";
 export default function Nav() {
     const [menuToggle, setMenuToggle]= useState(false);
 
-    function menuHandle() {
-        menuToggle ? setMenuToggle(false) : setMenuToggle(true);
-        
-        // menuToggle ? console.log("false!") : console.log("true!");
-    }
-
     return (
         <nav className={menuToggle ? "site-navigation toggled" : "site-navigation"} id="site-navigation">
             {/* hamburger menu button & svg */}
-            <button className="menu-toggle" aria-controls="header-menu" aria-expanded={menuToggle ? "true" : "false"} aria-label="Menu Toggle" onClick={() => menuHandle()}>
+            <button 
+                className="menu-toggle" 
+                aria-controls="header-menu" 
+                aria-expanded={menuToggle ? "true" : "false"} 
+                aria-label="Menu Toggle" 
+                onClick={() => {menuToggle ? setMenuToggle(false) : setMenuToggle(true) }}>
             <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
                 <title>Menu icon</title>
                 <path d="M2 5h20"></path>
@@ -26,10 +25,10 @@ export default function Nav() {
             {/* ul of nav links */}
             <ul className="nav-menu" id="header-menu">
                 <li>
-                    <Link to="/about" onClick={() => menuHandle()} >About</Link>
+                    <Link to="/about" onClick={() => setMenuToggle(false)} >About</Link>
                 </li>
                 <li>
-                    <Link to="/favourites" onClick={() => menuHandle()}>Favourites</Link>
+                    <Link to="/favourites" onClick={() => setMenuToggle(false)}>Favourites</Link>
                 </li>
             </ul>
         </nav>
